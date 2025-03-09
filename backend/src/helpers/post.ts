b61aa4372184a911edae9e364ac464375372ac3f
Caina <cycrohuh@gmail.com>
2025-03-09 18:46:05 -0300

    modified tsconfig

--- backend/src/helpers/post.ts
+++ backend/src/helpers/post.ts
@@ -16,26 +16,34 @@ export async function save_img(img: File, name?: string) {
 }
 
 export async function save_imgs(imgs: File | File[], target: ContentData) {
-  if (Array.isArray(imgs)) for (const img of imgs) target.imgs.push(await save_img(img));
-  else target.imgs.push(await save_img(imgs));
+  let r;
+  out: {
+    if (Array.isArray(imgs)) {
+      for (const img of imgs) if (r = await save_img(img)) target.imgs.push(r);
+      break out;
+    }
+    if (r = await save_img(imgs)) target.imgs.push(r);
+  }
 }
-
+interface Object {
+  keys<T>(o: T): keyof T;
+}
 async function get_prisma_contents(input: Addon) {
   const contents = [];
   const prisma_input = {} as Addon;
   let idx = 0;
   let content_idx = 0;
-  let key;
-  const keys = Object.keys(input);
+  let key: keyof Addon = "id";
+  const keys = Object.keys(input) as (keyof Addon)[];
   while (key = keys[idx++]) {
     if (key.startsWith("content")) {
       const elements: ContentData = {
-        text: input[key],
+        text: input[key] as any as string,
         imgs: [],
       };
       contents.push(elements);
       idx++;
-      await save_imgs(input[("img" + content_idx++)] as any as File | File[], elements);
+      await save_imgs(input[("img" + content_idx++) as keyof Addon] as any as File | File[], elements);
     } else if (key.startsWith("img")) {
       content_idx++;
       const elements: ContentData = {
@@ -46,11 +54,11 @@ async function get_prisma_contents(input: Addon) {
       contents.push(elements);
     }
     else if (key == "wallpaper" && input.wallpaper != void (0)) {
-      prisma_input.wallpaper = await save_img(input.wallpaper as any);
+      prisma_input.wallpaper = await save_img(input.wallpaper as any as File) as any as string;
     }
-    else if (key != "wallpaper") prisma_input[key] = input[key];
+    else if (key != "wallpaper") (prisma_input[key as keyof typeof prisma_input] as any) = input[key] as any as string;
   }
-  prisma_input.contents = {
+  (prisma_input as any).contents = {
     create: contents
   };
   return prisma_input;
@@ -59,7 +67,7 @@ async function get_prisma_contents(input: Addon) {
 export async function create_addon(input: Addon) {
   const contents = await get_prisma_contents(input);
   try {
-    const val = await prisma.addon.create({
+    await prisma.addon.create({
       data: contents
     });
     return "Ok";
@@ -78,7 +86,7 @@ export async function edit(id: number, input: Addon) {
     if (input.wallpaper && input.wallpaper != "undefined") {
       await delete_img(addon.wallpaper);
     } else {
-      delete input.wallpaper;
+      delete (input as any).wallpaper;
     }
     for (const content of addon.contents) {
       for (const img of content.imgs) {
