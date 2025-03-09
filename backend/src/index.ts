7762901eb44e09f5e1861e0cbb167d86cd2f2ae5
Caina <cycrohuh@gmail.com>
2025-03-09 19:02:33 -0300

    added / route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -56,6 +56,9 @@ async function main() {
       .post("/create", async ({ body }) => {
         return await create_addon(body as Addon)
       })
+      .get("/", (ctx) => {
+        ctx.body = "Já era";
+      })
       .listen(3000);
 }
 main();
